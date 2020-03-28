d7213fc7316bb1435905016046572fd740d79596
j <jwukusick@gmail.com>
2020-03-27 23:57:09 -0400

    Add spotify client auth

---
+++ spotify/clientAuth.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var {client_id, client_secret} = require('./config.js');
+
+var client_id = client_id; 
+var client_secret = client_secret; 
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/jmperezperez',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function (error, response, body) {
+      console.log(body);
+    });
+  }
+});
