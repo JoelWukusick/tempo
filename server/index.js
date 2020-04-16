981e4219dc6c7d93b1b6e045e56a0134036a31fd
j <jwukusick@gmail.com>
2020-04-16 17:42:27 -0400

    Remove unused server code

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const { client_id, redirect_uri } = require('../spotify/config.js');
 const cookieParser = require('cookie-parser');
 var favicon = require('serve-favicon');
 
-// var cors = require('cors');
-
 const port = 3000;
 const cache = {};
 const stateKey = 'spotify_auth_state';
@@ -122,41 +120,5 @@ app.get('/recommendations', (req, res) => {
       })
   }
 })
-
-app.get('/search/artist', (req, res) => {
-  client.findArtist(req.query.q, 10)
-    .then(result => {
-      res.send(result.artists.items)
-    });
-})
-
-app.get('/artists/:artistId/top-tracks', (req, res) => {
-  client.getTopTracks(req.params.artistId, 50)
-    .then(result => {
-      res.send(result.tracks)
-    });
-})
-
-app.get('/artists/:artistId/albums', (req, res) => {
-  client.getAlbums(req.params.artistId, 50)
-    .then(result => {
-      res.send(result.items)
-    });
-})
-
-app.get('/artists/:artistId/tracks', (req, res) => {
-  client.getAlbums(req.params.artistId, 50)
-    .then(result => {
-      albumIds = result.items.map(album => album.id);
-      return client.getTracks(albumIds);
-    })
-    .then((results) => {
-      return client.getTracksFeatures(results);
-    })
-    .then((tracks) => {
-      res.send(tracks)
-    })
-})
-
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- server/spotify.js
+++ server/spotify.js
@@ -2,7 +2,6 @@ const { tokenAsync, userAuthAsync } = require('../spotify/clientAuth.js');
 const rp = require('request-promise-native');
 const queryString = require('query-string');
 
-
 const getToken = (() => {
   let token;
   return async (refresh) => {
@@ -22,21 +21,6 @@ const getAuth = (() => {
     return token;
   }
 })();
-
-const constructBatchParams = (key, values) => {
-  let paramStrings = [];
-  let paramString = `${key}=`;
-  for (var i = 0; i < values.length; i++) {
-    paramString += values[i]
-    if ((i + 1) % 20 === 0 || i + 1 === values.length) {
-      paramStrings.push(paramString);
-      paramString = `${key}=`;
-    } else {
-      paramString += '%2C'
-    }
-  }
-  return paramStrings;
-}
 
 const getSpotify = async (url) => {
   return getToken()
@@ -75,66 +59,10 @@ const getSpotify = async (url) => {
     });
 }
 
-const getTrackBunch = async (paramString) => {
-  let url = `https://api.spotify.com/v1/albums?${paramString}`;
-  return getSpotify(url);
-}
-
-const getAllTracks = async (albumIds) => {
-  let paramStrings = constructBatchParams('ids', albumIds);
-  return Promise.all(paramStrings.map((paramString) => {
-    return getTrackBunch(paramString);
-  }))
-}
-
-const getTrackFeaturesBunch = async (paramString) => {
-  let url = `https://api.spotify.com/v1/audio-features?${paramString}`;
-  return getSpotify(url);
-}
-
-const getAllTracksFeatures = async (trackIds) => {
-  let paramStrings = constructBatchParams('ids', trackIds);
-  return Promise.all(paramStrings.map((paramString) => {
-    return getTrackFeaturesBunch(paramString);
-  }))
-}
-
 module.exports = {
-  findArtist: async (q, limit) => {
-    let url = `https://api.spotify.com/v1/search?q=${q}&type=artist${limit ? '&limit=' + limit : ''}`;
-    return getSpotify(url);
-  },
-  getAlbums: async (artistId, limit) => {
-    let url = `https://api.spotify.com/v1/artists/${artistId}/albums?market=US${limit ? '&limit=' + limit : ''}`;
-    return getSpotify(url);
-  },
   getGenreSeeds: async () => {
     let url = 'https://api.spotify.com/v1/recommendations/available-genre-seeds';
     return getSpotify(url);
-  },
-  getTopTracks: async (artistId, limit) => {
-    let url = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US${limit ? '&limit=' + limit : ''}`;
-    return getSpotify(url);
-  },
-  getTracks: async (albumIds) => {
-    return getAllTracks(albumIds)
-      .then(data => {
-        let albums = data.map(bunch => bunch.albums).flat();
-        let tracks = albums.map(album => album.tracks.items).flat();
-        tracks = tracks.map(track => { return { name: track.name, id: track.id } });
-        return tracks;
-      })
-  },
-  getTracksFeatures: async (tracks) => {
-    let trackIds = tracks.map(track => track.id);
-    return getAllTracksFeatures(trackIds)
-      .then(data => {
-        data = data.map(bunch => bunch.audio_features).flat();
-        for (var i = 0; i < tracks.length; i++) {
-          tracks[i].features = data[i];
-        }
-        return tracks;
-      })
   },
   getRecommendations: async (paramsObject) => {
     let paramsString = queryString.stringify(paramsObject);
